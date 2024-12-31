33e736d74ac874183bdb6afea49bd3e327c0eb4b
TrollGuy1 <lordoflords103@gmail.com>
2024-12-30 20:17:40 -0500

    V1.1

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/diagnostics/ArmDiagnostic.java
@@ -0,0 +1,37 @@
+package org.firstinspires.ftc.teamcode.diagnostics;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import org.firstinspires.ftc.teamcode.subsystems.Hardware;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+public class ArmDiagnostic extends LinearOpMode
+{
+    private DcMotor armSlideMotor, armRotateMotor;
+    private Telemetry telemetry;
+    private final double POWER = .7;
+    @Override
+    public void runOpMode() throws InterruptedException {
+        Hardware hardware = new Hardware();
+        hardware.hardwareMapDrivetrain(hardwareMap);
+
+        armSlideMotor = hardware.getArmSlideMotor();
+        armRotateMotor = hardware.getArmRotationMotor();
+
+
+
+        armSlideMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        armRotateMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+
+        waitForStart();
+        while(opModeIsActive())
+        {
+            armSlideMotor.setPower(gamepad1.dpad_up ? POWER : 0);
+            armSlideMotor.setPower(gamepad1.dpad_down ? -POWER : 0);
+            armRotateMotor.setPower(gamepad1.left_bumper ? POWER : 0);
+            armRotateMotor.setPower(gamepad1.right_bumper ? -POWER : 0);
+
+        }
+    }
+}
