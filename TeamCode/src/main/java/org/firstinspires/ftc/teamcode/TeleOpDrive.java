4148a4d48f05e29db838a2b4332f0ecca51e6d27
swomp7 <anttur2009@outlook.com>
2025-01-10 15:51:23 -0500

    updated teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpDrive.java
@@ -1,39 +1,32 @@
 package org.firstinspires.ftc.teamcode;
 
-
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
-
 @TeleOp(name="TeleOp Drive")
 
-
 public class TeleOpDrive extends LinearOpMode{
-
 
     private DcMotor frontLeftDrive;
     private DcMotor frontRightDrive;
     private DcMotor backLeftDrive;
     private DcMotor backRightDrive;
-
-
     private DcMotor armMotor;
+    private DcMotor armSlideMotor;
+    private DcMotor armRotateMotor;
 
 
     final private int BOTTOM = 0;
     final private int MIDDLE = 250;
     final private int TOP = 500;
 
-
     private CRServo leftServo;
     private CRServo rightServo;
 
-
     private final double POWER = 0.7;
-
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -42,45 +35,34 @@ public class TeleOpDrive extends LinearOpMode{
         backLeftDrive = hardwareMap.get(DcMotor.class,"bl");
         backRightDrive = hardwareMap.get(DcMotor.class,"br");
 
-
         leftServo = hardwareMap.get(CRServo.class,"ls");
         rightServo = hardwareMap.get(CRServo.class,"rs");
 
 
-
-
-        //armMotor = hardwareMap.get(DcMotor.class, "armMotor");
-
+        armSlideMotor = hardwareMap.get(DcMotor.class, "armSlideMotor");
+        armRotateMotor = hardwareMap.get(DcMotor.class, "armRotateMotor");
 
         frontLeftDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         frontRightDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         backLeftDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         backRightDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
-
         frontLeftDrive.setDirection(DcMotor.Direction.REVERSE);
         backLeftDrive.setDirection(DcMotor.Direction.REVERSE);
 
-
-        //armMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
-
-
+        armRotateMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         waitForStart();
-
 
         while (opModeIsActive()) {
             double drive = gamepad1.left_stick_y * 0.5;
             double strafe = gamepad1.left_stick_x * 0.7;
             double spin = -gamepad1.right_stick_x * 0.4;
 
-
             frontLeftDrive.setPower(-drive + strafe - spin);
             frontRightDrive.setPower(-drive - strafe + spin);
             backLeftDrive.setPower(drive - strafe - spin);
             backRightDrive.setPower(drive + strafe + spin);
-
 
             telemetry.addData("Front Left Motor Power", frontLeftDrive.getPower());
             telemetry.addData("Front Right Motor Power", frontRightDrive.getPower());
@@ -89,20 +71,31 @@ public class TeleOpDrive extends LinearOpMode{
             telemetry.update();
 
 
+            if (gamepad1.dpad_up)
+            {
+                armRotateMotor.setPower(0.5);
+            }
+            else if (gamepad1.dpad_down)
+            {
+                armRotateMotor.setPower(-0.5);
+            }
+            else
+            {
+                armRotateMotor.setPower(0);
+            }
 
-
-            // if (gamepad1.dpad_up)
-            // {
-            //     armMotor.setPower(0.5);
-            // }
-            // else if (gamepad1.dpad_down)
-            // {
-            //     armMotor.setPower(-0.5);
-            // }
-            // else
-            // {
-            //     armMotor.setPower(0);
-            // }
+            if (gamepad1.y)
+            {
+                armSlideMotor.setPower(0.5);
+            }
+            else if (gamepad1.a)
+            {
+                armSlideMotor.setPower(-0.5);
+            }
+            else
+            {
+                armSlideMotor.setPower(0);
+            }
 
 
             // if (gamepad1.a)
@@ -115,8 +108,6 @@ public class TeleOpDrive extends LinearOpMode{
             // }
 
 
-
-
             if(gamepad1.right_trigger > 0)
             {
                 leftServo.setPower(POWER);
@@ -125,7 +116,6 @@ public class TeleOpDrive extends LinearOpMode{
             {
                 rightServo.setPower(POWER);
             }
-
 
             if (gamepad1.a)
             {
@@ -146,23 +136,17 @@ public class TeleOpDrive extends LinearOpMode{
                 setIntakeMode("outtake");
             }
 
-
 //            telemetry.addData("Left Servo Power", leftServo.getPower());
 //            telemetry.addData("Right Servo Power", rightServo.getPower());
 //
 //            telemetry.addData("Left Servo Direction",leftServo.getDirection());
 //            telemetry.addData("Right Servo Direction",rightServo.getDirection());
 
-
             telemetry.update();
-
 
         }
 
-
     }
-
-
 
 
     public void armEncoderMovement(double power, int targetPosition, boolean logs) {
@@ -170,7 +154,6 @@ public class TeleOpDrive extends LinearOpMode{
         armMotor.setPower(power);
         armMotor.setTargetPosition(targetPosition);
         armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
 
         while (armMotor.isBusy()) {
             if (logs) {
@@ -182,10 +165,7 @@ public class TeleOpDrive extends LinearOpMode{
         armMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
 
-
-
     }
-
 
     public void setIntakeMode(String mode)
     {
@@ -200,7 +180,6 @@ public class TeleOpDrive extends LinearOpMode{
             rightServo.setDirection(CRServo.Direction.REVERSE);
         }
 
-
         if(leftServo.getPower() != 0)
         {
             leftServo.setPower(0);
@@ -212,7 +191,6 @@ public class TeleOpDrive extends LinearOpMode{
             rightServo.setPower(POWER);
         }
     }
-
 
 }
 
