8ac71254d899c03aae4eb0dc1a508da0f85aa391
Fishikaze <lordoflords103@gmail.com>
2025-02-04 21:46:05 -0500

    Added break statements to switch cases, fixed frontdrive errors and motorassignments

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonTest.java
@@ -4,46 +4,24 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.Servo;
 
 @Autonomous(name = "Auton")
-
-public class AutonTest extends LinearOpMode{
-    private DcMotor frontLeftDrive;
-    private DcMotor frontRightDrive;
-    private DcMotor backLeftDrive;
-    private DcMotor backRightDrive;
+public class AutonTest extends LinearOpMode {
+    private DcMotor frontLeftDrive, frontRightDrive, backLeftDrive, backRightDrive;
     private DcMotor armSlideMotor, armRotateMotor;
     private CRServo leftServo, rightServo;
-    private final int ROTATE = 500;
 
-    private enum Direction {
-        FORWARD,
-        BACKWARD,
-        RIGHT,
-        LEFT,
-    }
-    private enum armPos{
-        TOP,
-        BOTTOM
-    }
-    private enum armRotatePos{
-        TOP,
-        BOTTOM
-    }
-    private enum intakeMode
-    {
-        INTAKE,
-        OUTTAKE,
-        STOP
-    }
+    private enum Direction { FORWARD, BACKWARD, RIGHT, LEFT }
+    private enum armPos { TOP, BOTTOM }
+    private enum armRotatePos { TOP, BOTTOM }
+    private enum intakeMode { INTAKE, OUTTAKE, STOP }
 
     @Override
     public void runOpMode() throws InterruptedException {
-        frontLeftDrive = hardwareMap.get(DcMotor.class,"fl");
-        frontRightDrive = hardwareMap.get(DcMotor.class,"fr");
-        backLeftDrive = hardwareMap.get(DcMotor.class,"bl");
-        backRightDrive = hardwareMap.get(DcMotor.class,"br");
+        frontLeftDrive = hardwareMap.get(DcMotor.class, "fl");
+        frontRightDrive = hardwareMap.get(DcMotor.class, "fr");
+        backLeftDrive = hardwareMap.get(DcMotor.class, "bl");
+        backRightDrive = hardwareMap.get(DcMotor.class, "br");
 
         armSlideMotor = hardwareMap.get(DcMotor.class, "asm");
         armRotateMotor = hardwareMap.get(DcMotor.class, "arm");
@@ -63,145 +41,100 @@ public class AutonTest extends LinearOpMode{
 
         waitForStart();
 
-//        drive(500, 0.5, Direction.FORWARD);
-
-        // AUTON MOVEMENT HERE
-
-        // 4 DUNKS!!!
-
         setIntakeMode(intakeMode.INTAKE);
         armSlidePosition(500, 0.3, armPos.TOP);
         setIntakeMode(intakeMode.OUTTAKE);
         armSlidePosition(500, 0.3, armPos.BOTTOM);
-
     }
-    public void setIntakeMode(intakeMode intakeMode)
-    {
-        switch (intakeMode)
-        {
+
+    public void setIntakeMode(intakeMode mode) {
+        switch (mode) {
             case INTAKE:
                 leftServo.setPower(1);
                 rightServo.setPower(-1);
+                break;
             case OUTTAKE:
                 leftServo.setPower(-1);
                 rightServo.setPower(1);
+                break;
             case STOP:
                 leftServo.setPower(0);
                 rightServo.setPower(0);
-
+                break;
         }
-
     }
 
-    public void armSlidePosition(int position, double power, armPos armPos)
-    {
-        switch (armPos)
-        {
+    public void armSlidePosition(int position, double power, armPos pos) {
+        switch (pos) {
             case TOP:
-                armSlideMotor.setPower(power);
                 armSlideMotor.setTargetPosition(armSlideMotor.getCurrentPosition() + position);
-                armSlideMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                break;
             case BOTTOM:
-                armSlideMotor.setPower(-power);
                 armSlideMotor.setTargetPosition(armSlideMotor.getCurrentPosition() - position);
-                armSlideMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                break;
         }
-
+        armSlideMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        armSlideMotor.setPower(power);
     }
 
-    public void armRotatePosition(int position, double power, armRotatePos armRotatePos)
-    {
-        switch (armRotatePos)
-        {
+    public void armRotatePosition(int position, double power, armRotatePos pos) {
+        switch (pos) {
             case TOP:
-                armSlideMotor.setPower(power);
-                armSlideMotor.setTargetPosition(armSlideMotor.getCurrentPosition() + position);
-                armSlideMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                armRotateMotor.setTargetPosition(armRotateMotor.getCurrentPosition() + position);
+                break;
             case BOTTOM:
-                armSlideMotor.setPower(-power);
-                armSlideMotor.setTargetPosition(armSlideMotor.getCurrentPosition() - position);
-                armSlideMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                armRotateMotor.setTargetPosition(armRotateMotor.getCurrentPosition() - position);
+                break;
         }
-
+        armRotateMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        armRotateMotor.setPower(power);
     }
-    public void drive(int ticks, double power, Direction direction)
-    {
-        switch(direction) {
+
+    public void drive(int ticks, double power, Direction direction) {
+        switch (direction) {
             case FORWARD:
-                frontLeftDrive.setPower(power);
-                frontRightDrive.setPower(power);
-                backLeftDrive.setPower(power);
-                backRightDrive.setPower(power);
-
                 frontLeftDrive.setTargetPosition(frontLeftDrive.getCurrentPosition() + ticks);
-                frontRightDrive.setTargetPosition(frontLeftDrive.getCurrentPosition() + ticks);
-                backLeftDrive.setTargetPosition(frontLeftDrive.getCurrentPosition() + ticks);
-                backRightDrive.setTargetPosition(frontLeftDrive.getCurrentPosition() + ticks);
-
-                frontLeftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                frontRightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                backLeftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                backRightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                frontRightDrive.setTargetPosition(frontRightDrive.getCurrentPosition() + ticks);
+                backLeftDrive.setTargetPosition(backLeftDrive.getCurrentPosition() + ticks);
+                backRightDrive.setTargetPosition(backRightDrive.getCurrentPosition() + ticks);
+                break;
             case BACKWARD:
-                frontLeftDrive.setPower(power);
-                frontRightDrive.setPower(power);
-                backLeftDrive.setPower(power);
-                backRightDrive.setPower(power);
-
                 frontLeftDrive.setTargetPosition(frontLeftDrive.getCurrentPosition() - ticks);
-                frontRightDrive.setTargetPosition(frontLeftDrive.getCurrentPosition() - ticks);
-                backLeftDrive.setTargetPosition(frontLeftDrive.getCurrentPosition() - ticks);
-                backRightDrive.setTargetPosition(frontLeftDrive.getCurrentPosition() - ticks);
-
-                frontLeftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                frontRightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                backLeftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                backRightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                frontRightDrive.setTargetPosition(frontRightDrive.getCurrentPosition() - ticks);
+                backLeftDrive.setTargetPosition(backLeftDrive.getCurrentPosition() - ticks);
+                backRightDrive.setTargetPosition(backRightDrive.getCurrentPosition() - ticks);
+                break;
             case RIGHT:
-                frontLeftDrive.setPower(power);
-                frontRightDrive.setPower(power);
-                backLeftDrive.setPower(power);
-                backRightDrive.setPower(power);
-
                 frontLeftDrive.setTargetPosition(frontLeftDrive.getCurrentPosition() + ticks);
-                frontRightDrive.setTargetPosition(frontLeftDrive.getCurrentPosition() - ticks);
-                backLeftDrive.setTargetPosition(frontLeftDrive.getCurrentPosition() - ticks);
-                backRightDrive.setTargetPosition(frontLeftDrive.getCurrentPosition() + ticks);
-
-                frontLeftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                frontRightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                backLeftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                backRightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                frontRightDrive.setTargetPosition(frontRightDrive.getCurrentPosition() - ticks);
+                backLeftDrive.setTargetPosition(backLeftDrive.getCurrentPosition() - ticks);
+                backRightDrive.setTargetPosition(backRightDrive.getCurrentPosition() + ticks);
+                break;
             case LEFT:
-                frontLeftDrive.setPower(power);
-                frontRightDrive.setPower(power);
-                backLeftDrive.setPower(power);
-                backRightDrive.setPower(power);
-
                 frontLeftDrive.setTargetPosition(frontLeftDrive.getCurrentPosition() - ticks);
-                frontRightDrive.setTargetPosition(frontLeftDrive.getCurrentPosition() + ticks);
-                backLeftDrive.setTargetPosition(frontLeftDrive.getCurrentPosition() + ticks);
-                backRightDrive.setTargetPosition(frontLeftDrive.getCurrentPosition() - ticks);
-
-                frontLeftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                frontRightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                backLeftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                backRightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                frontRightDrive.setTargetPosition(frontRightDrive.getCurrentPosition() + ticks);
+                backLeftDrive.setTargetPosition(backLeftDrive.getCurrentPosition() + ticks);
+                backRightDrive.setTargetPosition(backRightDrive.getCurrentPosition() - ticks);
+                break;
         }
+        frontLeftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        frontRightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backLeftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backRightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
-
-
+        frontLeftDrive.setPower(power);
+        frontRightDrive.setPower(power);
+        backLeftDrive.setPower(power);
+        backRightDrive.setPower(power);
     }
-    public void robotFN()
-    {
+
+    public void robotFN() {
         armRotatePosition(1300, 0.3, armRotatePos.TOP);
-        armSlidePosition(1000,0.3,armPos.TOP);
+        armSlidePosition(1000, 0.3, armPos.TOP);
         setIntakeMode(intakeMode.OUTTAKE);
         sleep(1000);
         setIntakeMode(intakeMode.STOP);
         armRotatePosition(1300, 0.3, armRotatePos.BOTTOM);
-        armSlidePosition(1000,0.3,armPos.BOTTOM);
-
+        armSlidePosition(1000, 0.3, armPos.BOTTOM);
     }
-
 }
