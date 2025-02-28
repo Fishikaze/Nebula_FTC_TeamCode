8b3e65f55524078a57af8f3ba4728519b531818e
swomp7 <anttur2009@outlook.com>
2025-02-28 15:37:55 -0500

    updated drive method for Auton

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonTest.java
@@ -7,21 +7,30 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 @Autonomous(name = "Auton")
 public class AutonTest extends LinearOpMode {
-    private DcMotor frontLeftDrive, frontRightDrive, backLeftDrive, backRightDrive;
+    private DcMotor fl, fr, bl, br;
     private DcMotor armSlideMotor, armRotateMotor;
     private CRServo leftServo, rightServo;
 
-    private enum Direction { FORWARD, BACKWARD, RIGHT, LEFT }
-    private enum armPos { TOP, BOTTOM }
-    private enum armRotatePos { TOP, BOTTOM }
-    private enum intakeMode { INTAKE, OUTTAKE, STOP }
+    public enum armPos { TOP, BOTTOM }
+    public enum armRotatePos { TOP, BOTTOM }
+    public enum intakeMode { INTAKE, OUTTAKE, STOP }
+
+    public void doAuton() {
+        drive(90, 0.7, 0, 1000);
+        sleep(5000);
+
+        setIntakeMode(intakeMode.INTAKE);
+        armSlidePosition(500, 0.3, armPos.TOP);
+        setIntakeMode(intakeMode.OUTTAKE);
+        armSlidePosition(500, 0.3, armPos.BOTTOM);
+    }
 
     @Override
     public void runOpMode() throws InterruptedException {
-        frontLeftDrive = hardwareMap.get(DcMotor.class, "fl");
-        frontRightDrive = hardwareMap.get(DcMotor.class, "fr");
-        backLeftDrive = hardwareMap.get(DcMotor.class, "bl");
-        backRightDrive = hardwareMap.get(DcMotor.class, "br");
+        fl = hardwareMap.get(DcMotor.class, "fl");
+        fr = hardwareMap.get(DcMotor.class, "fr");
+        bl = hardwareMap.get(DcMotor.class, "bl");
+        br = hardwareMap.get(DcMotor.class, "br");
 
         armSlideMotor = hardwareMap.get(DcMotor.class, "asm");
         armRotateMotor = hardwareMap.get(DcMotor.class, "arm");
@@ -29,22 +38,19 @@ public class AutonTest extends LinearOpMode {
         leftServo = hardwareMap.get(CRServo.class, "ls");
         rightServo = hardwareMap.get(CRServo.class, "rs");
 
-        frontLeftDrive.setDirection(DcMotor.Direction.REVERSE);
-        backLeftDrive.setDirection(DcMotor.Direction.REVERSE);
+        fl.setDirection(DcMotor.Direction.REVERSE);
+        bl.setDirection(DcMotor.Direction.REVERSE);
 
-        frontLeftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        frontRightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        backLeftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        backRightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        fl.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        fr.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        bl.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        br.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         armSlideMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         armRotateMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         waitForStart();
 
-        setIntakeMode(intakeMode.INTAKE);
-        armSlidePosition(500, 0.3, armPos.TOP);
-        setIntakeMode(intakeMode.OUTTAKE);
-        armSlidePosition(500, 0.3, armPos.BOTTOM);
+        doAuton();
     }
 
     public void setIntakeMode(intakeMode mode) {
@@ -90,42 +96,59 @@ public class AutonTest extends LinearOpMode {
         armRotateMotor.setPower(power);
     }
 
-    public void drive(int ticks, double power, Direction direction) {
-        switch (direction) {
-            case FORWARD:
-                frontLeftDrive.setTargetPosition(frontLeftDrive.getCurrentPosition() + ticks);
-                frontRightDrive.setTargetPosition(frontRightDrive.getCurrentPosition() + ticks);
-                backLeftDrive.setTargetPosition(backLeftDrive.getCurrentPosition() + ticks);
-                backRightDrive.setTargetPosition(backRightDrive.getCurrentPosition() + ticks);
-                break;
-            case BACKWARD:
-                frontLeftDrive.setTargetPosition(frontLeftDrive.getCurrentPosition() - ticks);
-                frontRightDrive.setTargetPosition(frontRightDrive.getCurrentPosition() - ticks);
-                backLeftDrive.setTargetPosition(backLeftDrive.getCurrentPosition() - ticks);
-                backRightDrive.setTargetPosition(backRightDrive.getCurrentPosition() - ticks);
-                break;
-            case RIGHT:
-                frontLeftDrive.setTargetPosition(frontLeftDrive.getCurrentPosition() + ticks);
-                frontRightDrive.setTargetPosition(frontRightDrive.getCurrentPosition() - ticks);
-                backLeftDrive.setTargetPosition(backLeftDrive.getCurrentPosition() - ticks);
-                backRightDrive.setTargetPosition(backRightDrive.getCurrentPosition() + ticks);
-                break;
-            case LEFT:
-                frontLeftDrive.setTargetPosition(frontLeftDrive.getCurrentPosition() - ticks);
-                frontRightDrive.setTargetPosition(frontRightDrive.getCurrentPosition() + ticks);
-                backLeftDrive.setTargetPosition(backLeftDrive.getCurrentPosition() + ticks);
-                backRightDrive.setTargetPosition(backRightDrive.getCurrentPosition() - ticks);
-                break;
-        }
-        frontLeftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        frontRightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        backLeftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        backRightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+    public void drive(double theta, double power, double turn, int distance) {
+        double sin = Math.sin(theta - Math.PI/4);
+        double cos = Math.cos(theta - Math.PI/4);
+        double max = Math.max(Math.abs(sin), Math.abs(cos));
 
-        frontLeftDrive.setPower(power);
-        frontRightDrive.setPower(power);
-        backLeftDrive.setPower(power);
-        backRightDrive.setPower(power);
+        double frontLeftPower = power * cos/max + turn;
+        double frontRightPower = power * sin/max - turn;
+        double backLeftPower = power * sin/max + turn;
+        double backRightPower = power * cos/max - turn;
+
+        double maxPower = Math.max(Math.max(Math.abs(frontLeftPower), Math.abs(frontRightPower)), Math.max(Math.abs(backLeftPower), Math.abs(backRightPower)));
+        if((power + Math.abs(turn))>1) {
+            frontLeftPower /= power + turn;
+            frontRightPower /= power + turn;
+            backLeftPower /= power + turn;
+            backRightPower /= power + turn;
+        }
+
+        fl.setTargetPosition((int)(distance * (Math.abs(frontLeftPower / maxPower))) + fl.getCurrentPosition());
+        fr.setTargetPosition((int)(distance * (Math.abs(frontRightPower / maxPower))) + fr.getCurrentPosition());
+        bl.setTargetPosition((int)(distance * (Math.abs(backLeftPower / maxPower))) + bl.getCurrentPosition());
+        br.setTargetPosition((int)(distance * (Math.abs(backRightPower / maxPower))) + br.getCurrentPosition());
+
+        fl.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        bl.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        fr.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        br.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        fl.setPower(frontLeftPower);
+        fr.setPower(frontRightPower);
+        bl.setPower(backLeftPower);
+        br.setPower(backRightPower);
+        while (fl.getPower() != 0 || fr.getPower() != 0 || bl.getPower() != 0 || br.getPower() != 0)
+        {
+
+            int flp = fl.getCurrentPosition();
+            int frp = fr.getCurrentPosition();
+            int blp = bl.getCurrentPosition();
+            int brp = br.getCurrentPosition();
+
+            fl.setPower(flp >= fl.getTargetPosition() - 10 && flp <= fl.getTargetPosition() + 10 ? 0 : frontLeftPower);
+            fr.setPower(frp >= fr.getTargetPosition() - 10 && frp <= fr.getTargetPosition() + 10 ? 0 : frontRightPower);
+            bl.setPower(blp >= bl.getTargetPosition() - 10 && blp <= bl.getTargetPosition() + 10 ? 0 : backLeftPower);
+            br.setPower(brp >= br.getTargetPosition() - 10 && brp <= br.getTargetPosition() + 10 ? 0 : backRightPower);
+
+        }
+    }
+
+    public void resetMotors() {
+        fl.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        bl.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        fr.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        br.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
     }
 
     public void robotFN() {
